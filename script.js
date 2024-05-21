468f47d3a550adad1fef0103ee51b56723a1732b
Kiran TJ <166934975+thewelshfiend@users.noreply.github.com>
2024-05-21 09:16:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function firstWord(s) {
-  // your code here
+    // your code here
+    let S = s.trimStart();
+    const [start, end] = [0, S.indexOf(' ')];
+    if (end != -1)
+    {
+        return S.slice(start, end);
+    }
+    return S;
 }
+
+console.log(firstWord(``));
 
 // Do not change the code below
 
